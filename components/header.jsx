0430477a88747ce63fdd316c4a3470b49ac61f98
unknown <kabik5095@gmail.com>
2026-03-18 00:16:34 +0530

    added the authentication setup

--- components/header.jsx
+++ components/header.jsx
@@ -1,5 +1,20 @@
+import { SignInButton,  UserButton, Show} from "@clerk/nextjs";
+
 const Header = () => {
-    return <div>Header</div>;
+    return ( 
+    <div  className ="fixed top-0">
+        <Show when="signed-out">
+            <SignInButton />
+            {/* <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+            </SignUpButton> */}
+        </Show>
+        <Show when="signed-in">
+            <UserButton />
+        </Show>
+    </div>);
 };
 
 export default Header;
